234a3138550d3429b1465ab5e2e55ff6c7d8c6e5
tongnamuu <thban33@naver.com>
2020-09-07 23:39:07 +0900

    Fix Image.py

--- image.py
+++ image.py
@@ -1,16 +1,18 @@
 from PIL import Image
 import os
 
+
 def make_result(n: int, name: str):
-    img = Image.open('./screenshots/00.png')
+    img = Image.open('./screenshots/000.png')
     file_list = sorted(os.listdir('./screenshots'),)
     result_img = Image.new('RGB', (img.width, img.height*n))
     for idx, file in enumerate(file_list):
         img = Image.open(f'screenshots/{file}')
         result_img.paste(img, (0, idx*img.height))
-    result_width = result_img.width  // 2
+    result_width = result_img.width // 2
     result_img = result_img.crop((0, 0, result_width, result_img.height))
     result_img.save(f'result/{name}.jpg', optimize=True, quality=95)
+
 
 def init():
     clean('./result')
